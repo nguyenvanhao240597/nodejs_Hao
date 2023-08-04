e0d957667fc6dec922d121e707672fed9a35529f
nguyenvanhao240597 <informationtechnology.han9x@gmail.com>
2023-08-04 13:56:38 +0700

    upload

--- src/index.js
+++ src/index.js
@@ -9,6 +9,9 @@ var morgan = require('morgan');
 const app = express();
 const fs = require('fs');
 
+//Xem img
+app.use(express.static(path.join(__dirname, 'public')));
+
 //HTTP logger
 app.use(morgan('combined'));
 
